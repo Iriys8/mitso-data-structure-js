a1bd6cc0122f91c60a02149b1d7341008933ee7d
FIlyakin Maxim <iriys8@yandex.ru>
2024-12-05 03:24:14 +0300

    [feature] bloom-filter complete

--- src/bloom-filter.js
+++ src/bloom-filter.js
@@ -4,7 +4,11 @@ module.exports = class BloomFilter {
   /**
    * @param {number} size - the size of the storage.
    */
-  constructor() {
+  constructor(size) {
+    this.size = size;
+    this.num_element = 0; 
+    this.hashCount = 0; 
+    this.createStore(this.size);
     // Bloom filter size directly affects the likelihood of false positives.
     // The bigger the size the lower the likelihood of false positives.
   }
@@ -12,8 +16,13 @@ module.exports = class BloomFilter {
   /**
    * @param {string} item
    */
-  insert(/* item */) {
-    throw new NotImplementedError("Not implemented");
+  insert(item) {
+    this.num_element++;
+    this.hashCount = Math.ceil((this.size/this.num_element)* Math.log(2));
+    this.hahahahahehehehehehashes(item).forEach(hashValue => { 
+      this.bitArray[hashValue] = 1; 
+    });
+    return;
     // remove line with error and write your code here
   }
 
@@ -21,8 +30,8 @@ module.exports = class BloomFilter {
    * @param {string} item
    * @return {boolean}
    */
-  mayContain(/* item */) {
-    throw new NotImplementedError("Not implemented");
+  mayContain(item) {
+    
     // remove line with error and write your code here
   }
 
@@ -35,8 +44,39 @@ module.exports = class BloomFilter {
    * @param {number} size
    * @return {Object}
    */
-  createStore(/* size */) {
-    throw new NotImplementedError("Not implemented");
+  createStore(size) { // ???????? !!!!!!! &&&&&&&&
+    this.bitArray = new Array(size).fill(0);
+    return;
+    // remove line with error and write your code here
+  }
+  
+  hahahahahehehehehehashes(item){
+    this.hashArray = new Array(this.hashCount)
+    for (let i = 0; i < this.hashCount; i++){
+      switch(i){
+        case 0:
+          this.hashArray[i] = this.hash1(item);
+          break;
+        case 1:
+          this.hashArray[i] = this.hash2(item);
+          break;
+        default:
+          this.hashArray[i] = this.hash3(item, i);
+          break;
+      }
+    }
+    return this.hashArray;
+  }
+
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash1(item) {
+    const crypto = require('crypto'); 
+    const hash = crypto.createHash('sha256'); 
+    hash.update(item); 
+    return parseInt(hash.digest('hex'), 16) % this.size;
     // remove line with error and write your code here
   }
 
@@ -44,8 +84,11 @@ module.exports = class BloomFilter {
    * @param {string} item
    * @return {number}
    */
-  hash1(/* item */) {
-    throw new NotImplementedError("Not implemented");
+  hash2(item) {
+    const crypto = require('crypto'); 
+    const hash = crypto.createHash('md5'); 
+    hash.update(item); 
+    return parseInt(hash.digest('hex'), 16) % this.size;
     // remove line with error and write your code here
   }
 
@@ -53,17 +96,18 @@ module.exports = class BloomFilter {
    * @param {string} item
    * @return {number}
    */
-  hash2(/* item */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
-
-  /**
-   * @param {string} item
-   * @return {number}
-   */
-  hash3(/* item */) {
-    throw new NotImplementedError("Not implemented");
+  hash3(item , i) {
+    switch(i%3){
+      case 0:
+        const crypto = require('crypto'); 
+        const hash = crypto.createHash('sha1'); 
+        hash.update(item+i); 
+        return parseInt(hash.digest('hex'), 16) % this.size;
+      case 1:
+        return this.hash1(item+i);
+       case 2:
+        return this.hash2(item+i);
+   }
     // remove line with error and write your code here
   }
 
@@ -73,8 +117,22 @@ module.exports = class BloomFilter {
    * @param {string} item
    * @return {number[]}
    */
-  getHashValues(/* item */) {
-    throw new NotImplementedError("Not implemented");
+  getHashValues(item) {
+    return this.hahahahahehehehehehashes(item).every(hashValue => this.bitArray[hashValue] === 1);
     // remove line with error and write your code here
   }
 };
+
+/*
+And as we fall,
+The spirit carries on,
+THAT A HERO COME AND SAAVE US ALL!!!!!!!!!!!!
+As we call, 
+The ones we left below,
+WE ALL DREAM OFF THE DAAY WE RISE ABOVE!!!!!!!!!!!
+*/
+
+const bloom = new BloomFilter(100); 
+bloom.insert('da'); 
+console.log(bloom.getHashValues('da')); 
+console.log(bloom.getHashValues('net'));
